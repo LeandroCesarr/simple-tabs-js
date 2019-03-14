1ea50e60a91574f0cdd8b0c1a2c5998290d3493d
Leandro C. Silva <leandro.cesar@nohahaus.com.br>
2019-03-14 16:51:47 +0000

    Prepare stack for developer

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel';
+import minify from 'rollup-plugin-babel-minify';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+import scss from 'rollup-plugin-scss'
+
+const babelConfig = { exclude: 'node_modules/**' };
+const minifyConfig = { comments: false };
+
+export default [
+  {
+    input: 'resources/index.js',
+    output: {
+      name: 'index',
+      file: 'dist/index.min.js',
+      format: 'umd'
+    },
+    interop: false,
+    plugins: [
+      babel(babelConfig),
+      minify(minifyConfig),
+      uglify()
+    ]
+  },
+  {
+    input: 'resources/style.js',
+    output: {
+      file: 'dist/bundle.js',
+      format: "system"
+    },
+    plugins: [
+      scss() // will output compiled styles to bundle.css
+    ]
+  }
+]
